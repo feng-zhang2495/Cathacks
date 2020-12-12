d16e6b26fe28ef826ec6cc57ca73a637d13e7b72
feng-zhang2495 <73557467+feng-zhang2495@users.noreply.github.com>
2020-12-11 21:11:40 -0500

    First commit

---
+++ game.py
@@ -0,0 +1,56 @@
+import math 
+import random 
+import time 
+import pygame
+import sys 
+
+
+FPS = 60
+
+pygame.init()
+
+win = pygame.display.set_mode((640,480)) 
+pygame.display.set_caption("First Game")
+
+#character info
+x = 50
+y = 50
+width = 40
+height = 60
+vel = 5
+run = True
+
+#whatever this is
+clock = pygame.time.Clock()
+
+#Images
+ZebraB = pygame.image.load(r"assets\images\Zebra.png")
+GreenB = pygame.image.load(r"assets\images\Green.png")
+PinkB = pygame.image.load(r"assets\images\pink.png")
+RedB = pygame.image.load(r"assets\images\Red.png")
+YellowB = pygame.image.load(r"assets\images\yellow.png")
+
+mainMenupic = pygame.image.load(r'assets\images\Christmas.png') 
+#Music 
+Main_Menu_Music = pygame.mixer.music.load(r'assets\music\Joy_To_The_World_-_Instrumental_with_Lyrics_no_vocals.mp3')
+Map_Selection_Music = pygame.mixer.music.load(r'assets\music\Feliz_Navidad_Instrumental.mp3')
+Battle_Music = pygame.mixer.music.load(r'assets\music\Silent_Night_rock_instrumental_-_Zachary_Russell.mp3')
+Battle_Music2 = pygame.mixer.music.load(r'assets\music\Jingle_Bells_-_Merry_Heavy_Metal_Christmas.mp3')
+
+onMenu = True 
+
+#Map
+ice_map = pygame.image.load(r"assets\images\Frozen Ice Map.png")
+
+# win.blit(ice_map, (0, 0))
+
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit() 
+            run = False 
+    win.blit(ZebraB, (0, 0))
+    pygame.display.update()
+    clock.tick(FPS) 
+
